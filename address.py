b7ab32cec3b095eb8980e53b1e221d704e0d3a1f
grigori260694787898-ai <grigori260694787898@gmail.com>
2026-09-08 19:55:45 +0300

    Update address.py

--- address.py
+++ address.py
@@ -6,8 +6,8 @@ class Address:
         self.house = house
         self.apartment = apartment
     def __str__(self):
-        return f"{self.index} {self.city}, {self.street}, {self.house}, {self.apartment}"
-
+        return f"{self.index} {self.city}, {self.street}, {self.house} дом, квартира: {self.apartment}"
+ 
 
 
 
