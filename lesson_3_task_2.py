aaec47024e5af721f3cfe29654090f140f14e04e
grigori260694787898-ai <grigori260694787898@gmail.com>
2026-09-08 19:56:58 +0300

    Update lesson_3_task_2.py

--- lesson_3_task_2.py
+++ lesson_3_task_2.py
@@ -3,8 +3,11 @@ from smartphone import Smartphone
 catalog = [
    Smartphone("Apple", "iPhone 15", "+1-234-567-8901"),
    Smartphone("Samsung", "Galaxy S24", "+1-987-654-3210"),
-   Smartphone("Xiaomi", "14 Pro", "+7-999-111-2233")
+   Smartphone("Nokia", "2256", "+7-987-123-2244"),
+   Smartphone("Sony Erection", "ProLap", "+7-789-213-2255"),
+   Smartphone("LG", "G123", "+7-989-321-2266")
 ]
+
 
 for smartphone in catalog:
     print(f"{smartphone.brand} - {smartphone.model} - {smartphone.phone_number}")
